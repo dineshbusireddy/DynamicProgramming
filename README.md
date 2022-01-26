d51d82e36e1454169b2c4161228c464fad672f67
dinesh <dineshreddy.b@yahoo.com>
2022-01-26 22:58:00 +0530

    Add two numbers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DynamicProgramming
 
-#1. Two Sum
+##1. Two Sum
 Given an array of integers _nums_ and an integer _target_, return indices of the two numbers such that they add up to _target_.
 
 You may assume that each input would have *exactly one solution*, and you may not use the same element twice.
@@ -35,3 +35,35 @@ Output: [0,1]
 
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+##2. Add Two Numbers
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+
+**Example 1:**
+
+![Alt text](src/main/resources/Add Two Numbers Example.png?raw=true "Title")
+````
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+````
+**Example 2:**
+````
+Input: l1 = [0], l2 = [0]
+Output: [0]
+````
+**Example 3:**
+````
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+````
+
+**Constraints:**
+- The number of nodes in each linked list is in the range [1, 100].
+- 0 <= Node.val <= 9
+- It is guaranteed that the list represents a number that does not have leading zeros.
---
+++ src/main/java/com/dynamicprogramming/addtwonumbers/Main.java
@@ -0,0 +1,36 @@
+package com.dynamicprogramming.addtwonumbers;
+
+public class Main {
+    public static void main(String[] args) {
+        addTwoNumbers();
+    }
+
+    private static void addTwoNumbers() {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode listNodeOne = new ListNode(2).addLast(4).addLast(3);
+        ListNode listNodeTwo = new ListNode(5).addLast(6).addLast(4);
+        printOutput(addTwoNumbers.addTwoNumbers(listNodeOne, listNodeTwo));
+
+        listNodeOne = new ListNode(2).addLast(4).addLast(3).addLast(3);
+        listNodeTwo = new ListNode(5).addLast(6).addLast(4);
+        printOutput(addTwoNumbers.addTwoNumbers(listNodeOne, listNodeTwo));
+
+        listNodeOne = new ListNode(9).addLast(9).addLast(9).addLast(9).
+                addLast(9).addLast(9).addLast(9).addLast(9).addLast(9);
+        listNodeTwo = new ListNode(9).addLast(9).addLast(9).addLast(9);
+        printOutput(addTwoNumbers.addTwoNumbers(listNodeOne, listNodeTwo));
+
+        listNodeOne = new ListNode(0);
+        listNodeTwo = new ListNode(0);
+        printOutput(addTwoNumbers.addTwoNumbers(listNodeOne, listNodeTwo));
+    }
+
+    private static void printOutput(ListNode output) {
+        ListNode current = output;
+        while (current != null) {
+            System.out.print(current.val);
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
