4cb5c56c1d5318d93aeebc7e98981d996179eb0f
dinesh <dineshreddy.b@yahoo.com>
2022-01-28 21:47:15 +0530

    Longest Common Prefix

---
+++ src/main/java/com/dynamicprogramming/longest_common_prefix/LongestCommonPrefix.java
@@ -0,0 +1,39 @@
+package com.dynamicprogramming.longest_common_prefix;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        if (strs.length > 1) {
+            for (int i = 0; i < strs[0].length(); i++) {
+                char character = strs[0].charAt(i);
+                boolean isCommonPrefix = true;
+                for (int j = 1; j < strs.length; j++) {
+                    if (i >= strs[j].length() || strs[j].charAt(i) != character) {
+                        isCommonPrefix = false;
+                        break;
+                    }
+                }
+
+                if (isCommonPrefix) {
+                    result.append(character);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            result.append(strs[0]);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix solution = new LongestCommonPrefix();
+        System.out.println(solution.longestCommonPrefix(new String[]{"flower", "flow", "flight"}));// fl
+        System.out.println(solution.longestCommonPrefix(new String[]{"dog", "racecar", "car"}));//
+        System.out.println(solution.longestCommonPrefix(new String[]{"dog", "", "doe"}));//
+        System.out.println(solution.longestCommonPrefix(new String[]{"cir", "car"}));//c
+        System.out.println(solution.longestCommonPrefix(new String[]{"hello"}));//hello
+    }
+
+}
