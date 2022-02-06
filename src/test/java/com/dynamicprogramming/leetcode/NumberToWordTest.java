e44aa2b5f621f4f6a57925d5e99ac198cc3e44d7
dinesh <dineshreddy.b@yahoo.com>
2022-02-06 11:01:43 +0530

    Number To Word

--- src/test/java/com/dynamicprogramming/leetcode/NumberToWordTest.java
+++ src/test/java/com/dynamicprogramming/leetcode/NumberToWordTest.java
@@ -52,7 +52,25 @@ class NumberToWordTest {
                 Arguments.of(1, "One"),
                 Arguments.of(200, "Two Hundred"),
                 Arguments.of(56345672, "Five Crore Sixty Three Lakh Forty Five Thousand Six Hundred and Seventy Two"),
-                Arguments.of(5672, "Five Thousand Six Hundred and Seventy Two")
+                Arguments.of(5672, "Five Thousand Six Hundred and Seventy Two"),
+                Arguments.of(99, "Ninety Nine"),
+                Arguments.of(999, "Nine Hundred and Ninety Nine"),
+                Arguments.of(9999, "Nine Thousand Nine Hundred and Ninety Nine"),
+                Arguments.of(99999, "Ninety Nine Thousand Nine Hundred and Ninety Nine"),
+                Arguments.of(999999, "Nine Lakh Ninety Nine Thousand Nine Hundred and Ninety Nine"),
+                Arguments.of(56945781, "Five Crore Sixty Nine Lakh Forty Five Thousand Seven Hundred and Eighty One"),
+                Arguments.of(999999999, "Ninety Nine Crore Ninety Nine Lakh Ninety Nine Thousand Nine Hundred and Ninety Nine"),
+                Arguments.of(246864256, "Twenty Four Crore Sixty Eight Lakh Sixty Four Thousand Two Hundred and Fifty Six"),
+                Arguments.of(931945251, "Ninety Three Crore Nineteen Lakh Forty Five Thousand Two Hundred and Fifty One"),
+                Arguments.of(461450510, "Forty Six Crore Fourteen Lakh Fifty Thousand Five Hundred and Ten"),
+                Arguments.of(280765944, "Twenty Eight Crore Seven Lakh Sixty Five Thousand Nine Hundred and Forty Four"),
+                Arguments.of(843919757, "Eighty Four Crore Thirty Nine Lakh Nineteen Thousand Seven Hundred and Fifty Seven"),
+                Arguments.of(27903936, "Two Crore Seventy Nine Lakh Three Thousand Nine Hundred and Thirty Six"),
+                Arguments.of(539123533, "Fifty Three Crore Ninety One Lakh Twenty Three Thousand Five Hundred and Thirty Three"),
+                Arguments.of(13595904, "One Crore Thirty Five Lakh Ninety Five Thousand Nine Hundred and Four"),
+                Arguments.of(467257040, "Forty Six Crore Seventy Two Lakh Fifty Seven Thousand Forty"),
+                Arguments.of(179242024, "Seventeen Crore Ninety Two Lakh Forty Two Thousand Twenty Four"),
+                Arguments.of(262269097, "Twenty Six Crore Twenty Two Lakh Sixty Nine Thousand Ninety Seven")
         );
     }
 
