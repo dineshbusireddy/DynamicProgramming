3d793bf1b4445b86ff55d7293b5be6427215d805
dinesh <dineshreddy.b@yahoo.com>
2022-02-07 15:20:16 +0530

    Add Two Numbers

--- src/test/java/com/leetcode/AddTwoNumbersTest.java
+++ src/test/java/com/leetcode/AddTwoNumbersTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.StringJoiner;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,35 +17,25 @@ class AddTwoNumbersTest {
     @InjectMocks
     private AddTwoNumbers addTwoNumbers;
 
-    private static Stream<Arguments> testAddTwoNumbers() {
+    private static Stream<Arguments> testData() {
         return Stream.of(
-                Arguments.of(new ListNode(2).addLast(4).addLast(3),
-                        new ListNode(5).addLast(6).addLast(4),
-                        "[7, 0, 8]"),
-                Arguments.of(new ListNode(2).addLast(4).addLast(3).addLast(3),
-                        new ListNode(5).addLast(6).addLast(4),
-                        "[7, 0, 8, 3]"),
-                Arguments.of(new ListNode(9).addLast(9).addLast(9).addLast(9).
-                                addLast(9).addLast(9).addLast(9).addLast(9).addLast(9),
-                        new ListNode(9).addLast(9).addLast(9).addLast(9),
-                        "[8, 9, 9, 9, 0, 0, 0, 0, 0, 1]"),
-                Arguments.of(new ListNode(0), new ListNode(0), "[0]")
+                Arguments.of(ListNode.of(2, 4, 3),
+                        ListNode.of(5, 6, 4),
+                        ListNode.of(7, 0, 8)),
+                Arguments.of(ListNode.of(2, 4, 3, 3),
+                        ListNode.of(5, 6, 4),
+                        ListNode.of(7, 0, 8, 3)),
+                Arguments.of(ListNode.of(9, 9, 9, 9, 9, 9, 9, 9, 9),
+                        ListNode.of(9, 9, 9, 9),
+                        ListNode.of(8, 9, 9, 9, 0, 0, 0, 0, 0, 1)),
+                Arguments.of(ListNode.of(0), ListNode.of(0), ListNode.of(0))
         );
     }
 
     @ParameterizedTest
-    @MethodSource("testAddTwoNumbers")
-    void executeTests(ListNode l1, ListNode l2, String expected) {
-        assertEquals(expected, getActualAsString(addTwoNumbers.addTwoNumbers(l1, l2)));
+    @MethodSource("testData")
+    void testAddTwoNumbers(ListNode l1, ListNode l2, ListNode expected) {
+        assertEquals(ListNode.getAsString(expected), ListNode.getAsString(addTwoNumbers.addTwoNumbers(l1, l2)));
     }
 
-    private String getActualAsString(ListNode output) {
-        StringJoiner result = new StringJoiner(", ", "[", "]");
-        ListNode current = output;
-        while (current != null) {
-            result.add(current.val + "");
-            current = current.next;
-        }
-        return result.toString();
-    }
 }
