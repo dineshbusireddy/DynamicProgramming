e51cbc98b634c597b8593528c7abd3a7b4259b1e
dinesh <dineshreddy.b@yahoo.com>
2022-02-07 13:19:02 +0530

    Climbing Stairs

--- src/main/java/com/leetcode/ClimbingStairs.java
+++ src/main/java/com/leetcode/ClimbingStairs.java
@@ -1,7 +1,27 @@
 package com.leetcode;
 
 public class ClimbingStairs {
+
     public int climbStairs(int n) {
+        int result = 1;
+        if (n > 1) {
+            int one = 1;
+            int two = 1;
+            int index = n - 2;
+            while (index >= 0) {
+                int temp = one;
+                one = one + two;
+                two = temp;
+                --index;
+            }
+
+            result = one;
+        }
+
+        return result;
+    }
+
+    public int climbStairs1(int n) {
         int result = 1;
         if (n > 1) {
             int[] resultHolder = new int[n];
